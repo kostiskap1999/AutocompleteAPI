e955587700baed934bccb3c93290b7c29626acd8
kostiskap1999 <kostiskap1999@hotmail.com>
2025-09-22 20:24:06 +0300

    Small endpoint changes

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,9 @@ app.use(cors())
 app.use(express.json())
 
 const { autocompleteService } = require('./services/autocompleteService')
-app.get('/api/property/autocomplete', autocompleteService)
+app.get('/api/autocomplete', autocompleteService)
 
 const { postAd } = require('./services/adService')
-app.post('/api/property/ad', postAd)
+app.post('/api/ad', postAd)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
